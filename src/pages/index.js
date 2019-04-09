a5a94bd5ab8dfc938ce3044943960ac3a9f07ea0
unknown <allocompte@hotmail.fr>
2019-04-09 09:24:47 +0200

    Add introduction

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,19 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Accueil" />
+    <h1>Introduction</h1>
+    <p>
+    Actuellement, la France est l’un des pays les plus répressif concernant l’usage du cannabis. Bien qu’étant considéré comme une drogue douce, 
+    du point de vue de la loi, le cannabis n’est pas différencié des autres drogues. Pourtant les français comptent parmi ceux qui en consomment le plus. 
+    Ce constat permet aisément de rendre compte de l’ampleur de la controverse au sein même de notre société. En effet d’après les chiffres l’OFDT, 
+    bien que 54% des français pense que le cannabis puisse être dangereux dès le premier usage, 60% seraient d’avis de l’autoriser sous certaines conditions.
+    </p>
+
+    <p>
+    Avec l’arrivée récente du CBD dans certains commerces en France, le débat est ravivé avec de nombreux interviews et débats sur toute la sphère médiatique. 
+    De manière plus générale la succession de légalisations de la consommation et/ou de la vente dans un certain nombre de pays autour du globe génère une tendance 
+    mondiale.
+    </p>
   </Layout>
 )
 
